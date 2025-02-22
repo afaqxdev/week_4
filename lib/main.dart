adf125394e191e3217cd5dc7351ab8601eda82fc
Afaq Zahir <116549225+MetasenseDevelopment@users.noreply.github.com>
2025-02-22 12:03:17 +0500

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:week_4/animated_postion.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +10,87 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const AnimatedPositionScreen());
+    return ScreenUtilInit(
+      child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: Scaffold(
+            body: Center(
+                child: Column(
+              children: [
+                SizedBox(
+                  width: 100.w,
+                  height: 100.h,
+                )
+              ],
+            )),
+          )),
+    );
   }
 }
+
+// class SimlePainter extends CustomPainter {
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     final paint = Paint();
+//     paint.color = Colors.yellow;
+//     paint.style = PaintingStyle.fill;
+//     canvas.drawCircle(const Offset(30, 50), 150, paint);
+
+//     final eyePaint = Paint();
+//     eyePaint.color = Colors.black;
+//     eyePaint.style = PaintingStyle.fill;
+//     canvas.drawCircle(const Offset(0, 10), 30, eyePaint);
+
+//     canvas.drawCircle(const Offset(90, 10), 30, eyePaint);
+//     final simlePainter = Paint();
+//     simlePainter.color = Colors.black;
+//     simlePainter.style = PaintingStyle.stroke;
+//     canvas.drawArc(
+//         Rect.fromCenter(center: const Offset(40, 100), width: 200, height: 40),
+//         0.2,
+//         3,
+//         false,
+//         simlePainter..strokeWidth = 2);
+//   }
+
+//   @override
+//   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+// }
+
+// // Column(
+// //               mainAxisAlignment: MainAxisAlignment.center,
+// //               children: [
+// //                 CircleAvatar(
+// //                   backgroundColor: Colors.yellow,
+// //                   radius: 100,
+// //                   child: Column(
+// //                     mainAxisAlignment: MainAxisAlignment.center,
+// //                     crossAxisAlignment: CrossAxisAlignment.center,
+// //                     children: [
+// //                       Padding(
+// //                         padding: EdgeInsets.only(left: 60, bottom: 40),
+// //                         child: Row(
+// //                           children: [
+// //                             CircleAvatar(
+// //                               backgroundColor: Colors.black,
+// //                               radius: 15,
+// //                             ),
+// //                             SizedBox(
+// //                               width: 10,
+// //                             ),
+// //                             CircleAvatar(
+// //                               backgroundColor: Colors.black,
+// //                               radius: 15,
+// //                             ),
+// //                           ],
+// //                         ),
+// //                       )
+// //                     ],
+// //                   ),
+// //                 )
+// //               ],
+// //             ),
